7317dc24d1a1f478c82a3a60130ce2557877ceb2
makenaejiofor <152534282+makenaejiofor@users.noreply.github.com>
2024-02-18 08:18:56 +0300

    Updates

---
+++ precache-manifest.e1a8275ccc86ff052c6718336228a323.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "749bbd2948e09774fbdb17480b48c333",
+    "url": "/keppler/index.html"
+  },
+  {
+    "revision": "8ce8bb733f184278b327",
+    "url": "/keppler/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "bca861aa51c89950c9ff",
+    "url": "/keppler/static/css/main.30cf976e.chunk.css"
+  },
+  {
+    "revision": "8ce8bb733f184278b327",
+    "url": "/keppler/static/js/2.c9568678.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/keppler/static/js/2.c9568678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72a1d6199e92a5b964ad",
+    "url": "/keppler/static/js/3.c144a23f.chunk.js"
+  },
+  {
+    "revision": "bca861aa51c89950c9ff",
+    "url": "/keppler/static/js/main.6824fc5e.chunk.js"
+  },
+  {
+    "revision": "865b86a6216722ecf695",
+    "url": "/keppler/static/js/runtime-main.d7ccd149.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/keppler/static/media/SEGA.82e09f78.TTF"
+  }
+]);
