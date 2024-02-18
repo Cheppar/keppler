dd7a0f4b6ac3efcbe27cde7159452902b754a55f
makenaejiofor <152534282+makenaejiofor@users.noreply.github.com>
2024-02-18 07:30:32 +0300

    init

---
+++ src/robots.js
@@ -0,0 +1,32 @@
+export const robots = [
+    {
+        id: 0,
+        name: 'Cheppar Industries' ,
+        username: 'chepparing' ,
+        email: 'freeman@cheppar.co.ke'
+    },
+    {
+        id: 1,
+        name: 'Makena Ejiofor' ,
+        username: 'makenazie' ,
+        email: 'makena@cheppar.co.ke'
+    },
+    {
+        id: 2,
+        name: 'Mathia Weru' ,
+        username: 'mathias' ,
+        email: 'mathias@cheppar.co.ke'
+    },
+    {
+        id: 3,
+        name: 'Kanta McCraig' ,
+        username: 'macraigilo' ,
+        email: 'kanta@cheppar.co.ke'
+    },
+    {
+        id: 4,
+        name: 'Godfrey Cheppar' ,
+        username: 'goddeychep' ,
+        email: 'godfrey@cheppar.co.ke'
+    }
+]
