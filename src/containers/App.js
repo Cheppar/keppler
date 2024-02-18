b67eb9d30b3f54d223678b54fe045feed58d9d06
makenaejiofor <152534282+makenaejiofor@users.noreply.github.com>
2024-02-18 16:03:48 +0300

    With hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { useEffect, useState} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
@@ -11,50 +11,46 @@ import './App.css';
 //STATE >> props
 
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [], 
-            searchfield: ''
-        }
-        console.log('constructor');
-    }
+function App() {
 
-    componentDidMount(){
+    const [robots, setRobots] = useState ([]);
+    const [searchfield, setSearchfield] = useState ('');
+    const [count, setCount] = useState(0)
+
+    useEffect (()=> {
         fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState({robots: users }));
-        console.log('componentDidMount')
-    }
+                .then(response => response.json())
+                .then(users => {setRobots(users )});
+          //console.log(count)      
+    },[count]) // only run if count changes
 
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value});
-    }
-
-    render() { 
-        const {robots, searchfield} = this.state;
-        const filteredRobots = robots.filter(robot => {
+    const onSearchChange = (event) => {
+        setSearchfield(event.target.value);
+        }
+ 
+    const filteredRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         });
 
+   
+
         return !robots.length ? 
             <h1>Loading ...</h1> :
-        
              (
                 <div> 
                 <h1 className='f2'>Robo Team</h1>
-                <SearchBox searchChange={this.onSearchChange} />
+                <button onClick={()=>setCount(count+1)}>Tap Here</button>
+                <SearchBox searchChange={onSearchChange} />
                 <Scroll> 
                     <ErrorBoundry> 
-                <CardList robots={filteredRobots} />
-                </ErrorBoundry>
+                        <CardList robots={filteredRobots} />
+                    </ErrorBoundry>
                 </Scroll>
                 
                 </div>
             );    
              } 
-        }
+        
          
 
 export default App; 
